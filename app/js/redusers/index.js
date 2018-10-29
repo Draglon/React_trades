cc926b91b6b620953bc45b4b82cb5306904ae915
Sergey <s.nazarchuck@gmail.com>
2018-10-29 16:55:50 +0200

    add trading pages

--- app/js/redusers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import tracks from './tracks';
-import playlists from './playlists';
-
-export default combineReducers ({
-    tracks,
-    playlists
-});
