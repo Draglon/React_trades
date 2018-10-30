0850f6cdd4db184d0fad44a787189f15fda7ac10
Sergey <s.nazarchuck@gmail.com>
2018-10-30 15:24:30 +0200

    fix home page

--- app/containers/Home/index.jsx
+++ app/containers/Home/index.jsx
@@ -6,7 +6,8 @@ class Home extends Component {
         return (
             <section className='section invitation'>
                 <div className='wrapper'>
-                    <h1 className='invitation__title'>Invitation</h1>
+                    <h1 className='invitation__title'>Cryptocurrency Trading Platform</h1>
+                    <h2 className='invitation__subtitle'>OPEN ACCOUNT FOR FREE AND START TRADING NOW!</h2>
                 </div>
             </section>
         );
